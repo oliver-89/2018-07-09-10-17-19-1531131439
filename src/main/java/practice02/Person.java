dfebaba6a15e2b55f1f99e3b67e2ca7dcdf0ea29
OLIVER WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGOL2@OOCL.COM>
2020-07-22 16:42:16 +0800

    [oliver]feature:test1 finished

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,9 @@
 package practice02;
 
 public class Person {
+    private String name;
+    private int age;
+    public void introduce() {
+        System.out.println(" My name is "+name+". I am "+age+" years old.");
+    }
 }
