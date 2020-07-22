c5dee4172e0f9315b4278f4918e521676bb8373d
OLIVER WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGOL2@OOCL.COM>
2020-07-22 18:50:22 +0800

    [oliver]feature:test7 finished

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,21 @@
 package practice07;
 
-public class Student {
+public class Student<klass> extends Person{
+    private Klass klass;
+    public Student(String name, int age,Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        return  super.introduce()+" I am a Student. I am at Class "+klass.getNumber()+".";
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,42 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        String str="";
+        if(klass==null){
+            str= super.introduce()+" I am a Teacher. I teach No Class.";
+        }else {
+            str = super.introduce()+" I am a Teacher. I teach Class "+klass.getNumber()+".";
+        }
+        return  str;
+    }
+
+    public String introduceWith(Student student){
+        String str="";
+        if(student.getKlass().getNumber()==klass.getNumber()){
+            str = super.introduce()+" I am a Teacher. I teach "+student.getName()+".";
+        }else{
+            str = super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+        }
+        return str;
+    }
 }
