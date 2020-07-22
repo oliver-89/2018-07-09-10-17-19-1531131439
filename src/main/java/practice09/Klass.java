9eb8a22c6d7d265818f9b224821436ad38340f23
OLIVER WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGOL2@OOCL.COM>
2020-07-22 20:35:44 +0800

    [oliver]feature:test9 finished

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,45 @@
 package practice09;
 
+
+
 public class Klass {
+    private int number;
+    private String displayName;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class "+number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student student){
+        if(student.getKlass().getNumber()==this.number){
+            leader = student;
+
+        }else {
+            System.out.print("It is not one of us.\n");
+        }
+
+    }
+
+    public void appendMember(Student student){
+         student.getKlass().setNumber(number);
+    }
+
+
 }
